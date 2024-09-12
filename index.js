4a73e60d134ab61b9461cd8d772f341ac120e8da
Josiah Cavitana <jaypcavitana@gmail.com>
2024-09-12 11:53:33 +0800

    feat: added swagger documentation

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const express = require('express');
 const logger = require('./utilities/logger');
 const routes = require('./routes');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi/openapi.yml'); 
+
 const app = express();
 app.use(express.json());
+
+// Serve Swagger UI at /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(routes);
 
